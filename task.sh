ddf0730986f7ce6f5e3e13442da1a1f60a094574
pf70ex <rukai.yin@ing.com>
2021-06-23 11:56:38 +0200

    refactor: MatchInsert

--- task.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# parsing insert sql
-python ./src/LineageExtractor.py ./source/jsonAst/plsql_insert_1.json ./source/jsonAst/plsql_insert_2.json
---
+++ task_insert.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# parsing insert sql
+python ./src/MatchInsert.py ./source/jsonAst/insert/plsql_insert_1.json ./source/jsonAst/insert/plsql_insert_2.json
